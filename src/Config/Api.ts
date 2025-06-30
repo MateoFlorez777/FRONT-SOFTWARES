d78c9bb73bc91086c0484f7bfdacb18ca02e2e9c
MateoFlorez777 <juanmateopro77@gmail.com>
2025-06-30 16:46:49 -0500

    se cambia la ruta del back

--- src/Config/Api.ts
+++ src/Config/Api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = "https://back-softwares-production.up.railway.app";
+export const API_URL = "https://perfect-passion-production.up.railway.app";
 // change api
 
 export const api = axios.create({
