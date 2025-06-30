5520cb812a6e050eeb85d8c3cbdb98debfcab7a6
MateoFlorez777 <juanmateopro77@gmail.com>
2025-06-30 18:01:13 -0500

    deploy8

--- src/customer/pages/Home/TopBrands/Grid.tsx
+++ src/customer/pages/Home/TopBrands/Grid.tsx
@@ -39,58 +39,62 @@ const grid = [
       "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/13837166/2021/8/19/04e40e02-4c56-4705-94d0-f444b29973aa1629373611707-House-of-Pataudi-Women-Maroon-Embellished-Handcrafted-Wedges-1.jpg",
   },
 ];
+
+const placeholderImage = "https://via.placeholder.com/300x200?text=Imagen+no+disponible";
 const TopBrand = () => {
-  const {homePage}=useAppSelector(store=>store)
+  const { homePage } = useAppSelector((store) => store);
+
+  const grid = homePage.homePageData?.grid || [];
+
   return (
     <div className="grid gap-4 grid-rows-12 grid-cols-12 lg:h-[600px] px-5 lg:px-20">
-      <div className=" col-span-3 row-span-12  text-white  rounded ">
+      <div className="col-span-3 row-span-12 text-white rounded">
         <img
           className="w-full h-full object-cover border-fuchsia-800 lg:border-[9px]s rounded-md"
-          src={homePage.homePageData?.grid[0].image}
+          src={grid[0]?.image || placeholderImage}
           alt=""
         />
       </div>
 
-      <div className="col-span-2 row-span-6  text-white rounded">
+      <div className="col-span-2 row-span-6 text-white rounded">
         <img
           className="w-full h-full object-cover border-fuchsia-800 lg:border-[9px]s rounded-md"
-          src={homePage.homePageData?.grid[1].image}
+          src={grid[1]?.image || placeholderImage}
           alt=""
         />
       </div>
 
-      <div className="col-span-4 row-span-6  text-white  rounded ">
+      <div className="col-span-4 row-span-6 text-white rounded">
         <img
           className="w-full h-full object-cover object-top border-fuchsia-800 lg:border-[9px]s rounded-md"
-          src={homePage.homePageData?.grid[2].image}
+          src={grid[2]?.image || placeholderImage}
           alt=""
         />
       </div>
 
-      <div className="col-span-3 row-span-12  text-white  rounded ">
+      <div className="col-span-3 row-span-12 text-white rounded">
         <img
           className="w-full h-full object-cover object-top border-fuchsia-800 lg:border-[9px]s rounded-md"
-          src={homePage.homePageData?.grid[3].image}
+          src={grid[3]?.image || placeholderImage}
           alt=""
         />
       </div>
 
-      <div className="col-span-4 row-span-6  text-white  rounded ">
+      <div className="col-span-4 row-span-6 text-white rounded">
         <img
           className="w-full h-full object-cover object-top border-fuchsia-800 lg:border-[9px]s rounded-md"
-          src={homePage.homePageData?.grid[4].image}
+          src={grid[4]?.image || placeholderImage}
           alt=""
         />
       </div>
-      <div className="col-span-2 row-span-6  text-white rounded ">
+
+      <div className="col-span-2 row-span-6 text-white rounded">
         <img
           className="w-full h-full object-cover border-fuchsia-800 lg:border-[9px]s rounded-md"
-          src={homePage.homePageData?.grid[5].image}
+          src={grid[5]?.image || placeholderImage}
           alt=""
         />
       </div>
-
-      {/* https://tristenwallace.com/wp-content/uploads/2022/06/wed-7.jpg */}
     </div>
   );
 };
