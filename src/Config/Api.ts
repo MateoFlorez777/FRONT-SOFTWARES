bd1f5163d9ee67ee9512ca1c997de1da012cd3d9
MateoFlorez777 <juanmateopro77@gmail.com>
2025-06-30 14:56:20 -0500

    se agrega la ruta del back en railway

--- src/Config/Api.ts
+++ src/Config/Api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = "http://localhost:5454";
+export const API_URL = "https://back-softwares-production.up.railway.app";
 // change api
 
 export const api = axios.create({
