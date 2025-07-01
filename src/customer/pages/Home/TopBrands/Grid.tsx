b3d4d4c0d8d39e582c8ec8b43af898cb31ba9084
MateoFlorez777 <juanmateopro77@gmail.com>
2025-06-30 19:21:56 -0500

    deploy16

--- src/customer/pages/Home/TopBrands/Grid.tsx
+++ src/customer/pages/Home/TopBrands/Grid.tsx
@@ -6,7 +6,7 @@ const grid = [
         "section": "GRID",
         "name": "women lehenga cholis",
     image:
-      "https://http2.mlstatic.com/D_NQ_NP_972200-MLU74088420395_012024-O-audifonos-inalambricos-wh-ch520.webp",
+      "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/23807268/2023/6/29/9930b235-5318-4755-abbe-08f99e969e781688026636544LehengaCholi7.jpg",
   },
   {"categoryId":"men_formal_shoes",
         "section": "GRID",
@@ -30,7 +30,7 @@ const grid = [
         "section": "GRID",
         "name": "women jewellery",
     image:
-      "https://http2.mlstatic.com/D_NQ_NP_972200-MLU74088420395_012024-O-audifonos-inalambricos-wh-ch520.webp",
+      "https://media.istockphoto.com/id/1276740597/photo/indian-traditional-gold-necklace.jpg?b=1&s=612x612&w=0&k=20&c=S-QnNZKqf2u3L-GIaDiIinNRU74GBWQaIDwY7gYJboY=",
   },
   {"categoryId":"women_footwear",
         "section": "GRID",
@@ -38,75 +38,59 @@ const grid = [
     image:
       "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/13837166/2021/8/19/04e40e02-4c56-4705-94d0-f444b29973aa1629373611707-House-of-Pataudi-Women-Maroon-Embellished-Handcrafted-Wedges-1.jpg",
   },
-    {"categoryId":"women_footwear",
-        "section": "GRID",
-        "name": "women footwear",
-    image:
-      "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/13837166/2021/8/19/04e40e02-4c56-4705-94d0-f444b29973aa1629373611707-House-of-Pataudi-Women-Maroon-Embellished-Handcrafted-Wedges-1.jpg",
-  },
-    {"categoryId":"women_footwear",
-        "section": "GRID",
-        "name": "women footwear",
-    image:
-      "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/13837166/2021/8/19/04e40e02-4c56-4705-94d0-f444b29973aa1629373611707-House-of-Pataudi-Women-Maroon-Embellished-Handcrafted-Wedges-1.jpg",
-  },
 ];
-
-const placeholderImage = "https://via.placeholder.com/300x200?text=Imagen+no+disponible";
 const TopBrand = () => {
-  const { homePage } = useAppSelector((store) => store);
-
-  const grid = homePage.homePageData?.grid || [];
-
+  const {homePage}=useAppSelector(store=>store)
   return (
     <div className="grid gap-4 grid-rows-12 grid-cols-12 lg:h-[600px] px-5 lg:px-20">
-      <div className="col-span-3 row-span-12 text-white rounded">
+      <div className=" col-span-3 row-span-12  text-white  rounded ">
         <img
           className="w-full h-full object-cover border-fuchsia-800 lg:border-[9px]s rounded-md"
-          src={grid[0]?.image || placeholderImage}
+          src={homePage.homePageData?.grid[0].image}
           alt=""
         />
       </div>
 
-      <div className="col-span-2 row-span-6 text-white rounded">
+      <div className="col-span-2 row-span-6  text-white rounded">
         <img
           className="w-full h-full object-cover border-fuchsia-800 lg:border-[9px]s rounded-md"
-          src={grid[1]?.image || placeholderImage}
+          src={homePage.homePageData?.grid[1].image}
           alt=""
         />
       </div>
 
-      <div className="col-span-4 row-span-6 text-white rounded">
+      <div className="col-span-4 row-span-6  text-white  rounded ">
         <img
           className="w-full h-full object-cover object-top border-fuchsia-800 lg:border-[9px]s rounded-md"
-          src={grid[2]?.image || placeholderImage}
+          src={homePage.homePageData?.grid[2].image}
           alt=""
         />
       </div>
 
-      <div className="col-span-3 row-span-12 text-white rounded">
+      <div className="col-span-3 row-span-12  text-white  rounded ">
         <img
           className="w-full h-full object-cover object-top border-fuchsia-800 lg:border-[9px]s rounded-md"
-          src={grid[3]?.image || placeholderImage}
+          src={homePage.homePageData?.grid[3].image}
           alt=""
         />
       </div>
 
-      <div className="col-span-4 row-span-6 text-white rounded">
+      <div className="col-span-4 row-span-6  text-white  rounded ">
         <img
           className="w-full h-full object-cover object-top border-fuchsia-800 lg:border-[9px]s rounded-md"
-          src={grid[4]?.image || placeholderImage}
+          src={homePage.homePageData?.grid[4].image}
           alt=""
         />
       </div>
-
-      <div className="col-span-2 row-span-6 text-white rounded">
+      <div className="col-span-2 row-span-6  text-white rounded ">
         <img
           className="w-full h-full object-cover border-fuchsia-800 lg:border-[9px]s rounded-md"
-          src={grid[5]?.image || placeholderImage}
+          src={homePage.homePageData?.grid[5].image}
           alt=""
         />
       </div>
+
+      {/* https://tristenwallace.com/wp-content/uploads/2022/06/wed-7.jpg */}
     </div>
   );
 };
